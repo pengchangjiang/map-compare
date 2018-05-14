0bf285ce1daa0f9d204c3e2365e315c4552eb538
pengcj <pengchangjiang@geo-compass.com>
2018-05-14 15:59:13 +0800

    基础搭建

---
+++ js/data.js
@@ -0,0 +1,13 @@
+// const axios = require('axios.js');
+
+// async function _getData(url){
+//    let {data}= await axios.get(url);
+//    return data;
+// }
+
+// export default{
+//     5000:function(){
+//         let data = _getData('../data/5000points.json');
+//         return data;
+//     }
+// }
